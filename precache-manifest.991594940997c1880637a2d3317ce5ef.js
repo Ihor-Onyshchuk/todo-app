9b46c111853a445a3acc60cbc5c2bbb2076b44a9
ihor <onyshchukihor11@gmail.com>
2020-08-02 12:02:35 +0300

    Updates

---
+++ precache-manifest.991594940997c1880637a2d3317ce5ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "556928cec5a5ab68c1f42d4469e8ca54",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "e3bb3dcd7c78f4255501",
+    "url": "/todo-app/static/css/main.28c0883c.chunk.css"
+  },
+  {
+    "revision": "4ab788a041088683f36f",
+    "url": "/todo-app/static/js/2.1a3f1d65.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todo-app/static/js/2.1a3f1d65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3bb3dcd7c78f4255501",
+    "url": "/todo-app/static/js/main.5f329a04.chunk.js"
+  },
+  {
+    "revision": "aed0306b7a4838737999",
+    "url": "/todo-app/static/js/runtime-main.3c9e9223.js"
+  }
+]);
